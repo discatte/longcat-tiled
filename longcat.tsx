bafdf2edf7f76820513e54f6d00c7b845e9408e3
Galactic Discatte <63693994+discatte@users.noreply.github.com>
2021-04-21 08:44:33 -0700

    Add files via upload

---
+++ longcat.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="longcat" tilewidth="48" tileheight="48" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="48" height="48" source="longcat-w.png"/>
+ </tile>
+ <tile id="2">
+  <image width="48" height="48" source="longcat-s.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="48" source="longcatoverlap-we-ns.png"/>
+ </tile>
+ <tile id="4">
+  <image width="48" height="48" source="longcatoverlap-sw-e.png"/>
+ </tile>
+ <tile id="5">
+  <image width="48" height="48" source="longcatoverlap-se-w.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="48" source="longcatoverlap-nw-se.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="48" source="longcatoverlap-nw-e.png"/>
+ </tile>
+ <tile id="8">
+  <image width="48" height="48" source="longcatoverlap-ns-we.png"/>
+ </tile>
+ <tile id="9">
+  <image width="48" height="48" source="longcatoverlap-ne-w.png"/>
+ </tile>
+ <tile id="10">
+  <image width="48" height="48" source="longcatoverlap-ne-sw.png"/>
+ </tile>
+ <tile id="11">
+  <image width="48" height="48" source="longcatopus-swe.png"/>
+ </tile>
+ <tile id="12">
+  <image width="48" height="48" source="longcatopus-nwe.png"/>
+ </tile>
+ <tile id="13">
+  <image width="48" height="48" source="longcatopus-nsw.png"/>
+ </tile>
+ <tile id="14">
+  <image width="48" height="48" source="longcatopus-nswe.png"/>
+ </tile>
+ <tile id="15">
+  <image width="48" height="48" source="longcatopus-nse.png"/>
+ </tile>
+ <tile id="16">
+  <image width="48" height="48" source="longcat-n.png"/>
+ </tile>
+ <tile id="17">
+  <image width="48" height="48" source="longcatislong-we.png"/>
+ </tile>
+ <tile id="18">
+  <image width="48" height="48" source="longcatislong-sw.png"/>
+ </tile>
+ <tile id="19">
+  <image width="48" height="48" source="longcatislong-se.png"/>
+ </tile>
+ <tile id="20">
+  <image width="48" height="48" source="longcatislong-nw.png"/>
+ </tile>
+ <tile id="21">
+  <image width="48" height="48" source="longcatislong-ns.png"/>
+ </tile>
+ <tile id="22">
+  <image width="48" height="48" source="longcatislong-ne.png"/>
+ </tile>
+ <tile id="23">
+  <image width="48" height="48" source="longcat-e.png"/>
+ </tile>
+ <tile id="24">
+  <image width="48" height="48" source="longcatbeans-w.png"/>
+ </tile>
+ <tile id="25">
+  <image width="48" height="48" source="longcatbeans-s.png"/>
+ </tile>
+ <tile id="26">
+  <image width="48" height="48" source="longcatbeans-n.png"/>
+ </tile>
+ <tile id="27">
+  <image width="48" height="48" source="longcatbeans-e.png"/>
+ </tile>
+</tileset>
